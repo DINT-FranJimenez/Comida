87931c86271062229c6484e873a8a2102b04f797
FRANCISCO JIMÉNEZ LOZANO <frajimloz@alu.edu.gva.es>
2023-11-30 19:41:44 +0100

    Mirar el wrap, va raro

--- Comida/MainWindow.xaml.cs
+++ Comida/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,7 @@ namespace Comida
         private string pathImage = "C:\\Users\\nero_\\source\\repos\\Comida\\Comida\\Imagenes\\";
 
         private MainWindowVM vm;
+        private Plato selectedPlato;
 
 
         public MainWindow()
@@ -31,6 +33,135 @@ namespace Comida
             vm = new MainWindowVM(Plato.GetSamples(pathImage));
 
            this.DataContext = vm;
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            xTextName.IsEnabled = true;
+            xTextImage.IsEnabled = true;
+            xCheckBoxLeche.IsEnabled = true;
+            xCheckBoxSoja.IsEnabled = true;
+            xCheckBoxSulfitos.IsEnabled = true;
+            xCheckBoxGluten.IsEnabled = true;
+            xButtonDeselecciona.IsEnabled = true;
+            xComboBoxTipo.IsEnabled = true;
+
+            if (e.AddedItems.Count > 0) {
+
+                // Obtén el elemento seleccionado
+                selectedPlato = (Plato)e.AddedItems[0];
+                // Asigna el nombre del elemento seleccionado al TextBox
+                xTextName.Text = selectedPlato.Nombre;
+                xTextImage.Text = selectedPlato.Imagen;
+                xComboBoxTipo.Text = selectedPlato.Tipo;
+
+                if (selectedPlato.Leche) xCheckBoxLeche.IsChecked = true;
+                else xCheckBoxLeche.IsChecked = false;
+
+                if (selectedPlato.Soja) xCheckBoxSoja.IsChecked = true;
+                else xCheckBoxSoja.IsChecked = false;
+
+                if (selectedPlato.Sulfitos) xCheckBoxSulfitos.IsChecked = true;
+                else xCheckBoxSulfitos.IsChecked = false;
+
+                if (selectedPlato.Gluten) xCheckBoxGluten.IsChecked = true;
+                else xCheckBoxGluten.IsChecked = false;
+            }
+        }
+
+        private void Deseleccionar_Click(object sender, RoutedEventArgs e)
+        {
+            //Deseleccionamos el elemento del lixtbox
+            xListBoxPlatos.SelectedItem = null;
+
+            //Limpio los Text Box
+            xTextName.Text= "";
+            xTextImage.Text= "";
+            xComboBoxTipo.Text = "";
+
+            //Deselecciono botones
+            xCheckBoxLeche.IsChecked = false;
+            xCheckBoxSoja.IsChecked = false;
+            xCheckBoxSulfitos.IsChecked = false;
+            xCheckBoxGluten.IsChecked = false;
+
+            //Deshabilito los elementos
+            xTextName.IsEnabled = false;
+            xTextImage.IsEnabled = false;
+            xCheckBoxLeche.IsEnabled = false;
+            xCheckBoxSoja.IsEnabled = false;
+            xCheckBoxSulfitos.IsEnabled = false;
+            xCheckBoxGluten.IsEnabled = false;
+            xComboBoxTipo.IsEnabled = false;
+
+        }
+
+        private void Poner_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox =  sender as CheckBox;
+
+            if (xListBoxPlatos.SelectedItem != null)
+            {
+                switch (checkBox.Content.ToString())
+                {
+                    case "Soja":
+                        selectedPlato.Soja = true;
+                        break;
+                    case "Sulfitos":
+                        selectedPlato.Sulfitos = true;
+                        break;
+                    case "Leche":
+                        selectedPlato.Leche = true;
+                        break;
+                    case "Gluten":
+                        selectedPlato.Gluten = true;
+                        break;
+                }
+            }
+        }
+
+        private void Quitar_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+
+            if (xListBoxPlatos.SelectedItem != null)
+            {
+                switch (checkBox.Content.ToString())
+                {
+                    case "Soja":
+                        selectedPlato.Soja = false;
+                        break;
+                    case "Sulfitos":
+                        selectedPlato.Sulfitos = false;
+                        break;
+                    case "Leche":
+                        selectedPlato.Leche =false;
+                        break;
+                    case "Gluten":
+                        selectedPlato.Gluten = false;
+                        break;
+                }
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+
+            string selectedItemContent = (comboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+            switch (selectedItemContent)
+            {
+                case "Americana":
+                    selectedPlato.Tipo = "Americana";
+                    break;
+                case "China":
+                    selectedPlato.Tipo = "China";
+                    break;
+                case "Mexicana":
+                    selectedPlato.Tipo = "Mexicana";
+                    break;
+            }
         }
     }
 }
