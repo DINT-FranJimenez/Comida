1657eeb8d7102eb29710a386837386cdaef79e98
FRANCISCO JIMÉNEZ LOZANO <frajimloz@alu.edu.gva.es>
2023-12-04 12:47:00 +0100

    Actualizado hasta donde me da la cabeza, código C# reducido

--- Comida/MainWindow.xaml.cs
+++ Comida/MainWindow.xaml.cs
@@ -39,26 +39,10 @@ namespace Comida
             xButtonDeselecciona.IsEnabled = true;
             xComboBoxTipo.IsEnabled = true;
 
-            if (e.AddedItems.Count > 0) {
-
+            if (e.AddedItems.Count > 0)
+            {
                 // Obtén el elemento seleccionado 
-                vm.Plato = (Plato)e.AddedItems[0];
-                // Asigna el nombre del elemento seleccionado al TextBox
-                xTextName.Text = vm.Plato.Nombre;
-                xTextImage.Text = vm.Plato.Imagen;
-                xComboBoxTipo.Text = vm.Plato.Tipo;
-
-                if (vm.Plato.Leche) xCheckBoxLeche.IsChecked = true;
-                else xCheckBoxLeche.IsChecked = false;
-
-                if (vm.Plato.Soja) xCheckBoxSoja.IsChecked = true;
-                else xCheckBoxSoja.IsChecked = false;
-
-                if (vm.Plato.Sulfitos) xCheckBoxSulfitos.IsChecked = true;
-                else xCheckBoxSulfitos.IsChecked = false;
-
-                if (vm.Plato.Gluten) xCheckBoxGluten.IsChecked = true;
-                else xCheckBoxGluten.IsChecked = false;
+                vm.Plato = (Plato)e.AddedItems[0];                  
             }
         }
 
@@ -90,73 +74,5 @@ namespace Comida
 
         }
 
-        private void Poner_Checked(object sender, RoutedEventArgs e)
-        {
-            
-            CheckBox checkBox =  sender as CheckBox;
-
-            if (xListBoxPlatos.SelectedItem != null)
-            {
-                switch (checkBox.Content.ToString())
-                {
-                    case "Soja":
-                        vm.Plato.Soja = true;
-                        break;
-                    case "Sulfitos":
-                        vm.Plato.Sulfitos = true;
-                        break;
-                    case "Leche":
-                        vm.Plato.Leche = true;
-                        break;
-                    case "Gluten":
-                        vm.Plato.Gluten = true;
-                        break;
-                }
-            }
-        }
-
-        private void Quitar_Unchecked(object sender, RoutedEventArgs e)
-        {
-            CheckBox checkBox = sender as CheckBox;
-
-            if (xListBoxPlatos.SelectedItem != null)
-            {
-                switch (checkBox.Content.ToString())
-                {
-                    case "Soja":
-                        vm.Plato.Soja = false;
-                        break;
-                    case "Sulfitos":
-                        vm.Plato.Sulfitos = false;
-                        break;
-                    case "Leche":
-                        vm.Plato.Leche =false;
-                        break;
-                    case "Gluten":
-                        vm.Plato.Gluten = false;
-                        break;
-                }
-            }
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBox comboBox = sender as ComboBox;
-
-            string selectedItemContent = (comboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
-
-            switch (selectedItemContent)
-            {
-                case "Americana":
-                    vm.Plato.Tipo = "Americana";
-                    break;
-                case "China":
-                    vm.Plato.Tipo = "China";
-                    break;
-                case "Mexicana":
-                    vm.Plato.Tipo = "Mexicana";
-                    break;
-            }
-        }
     }
 }
